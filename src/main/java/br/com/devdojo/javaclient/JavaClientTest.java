a745cfc8959140ddcc9d7ba0f4b927506f183792
Artur Lansoni <arturlansoni@gmail.com>
2020-03-31 16:42:03 -0300

    Create native Java request

--- src/main/java/br/com/devdojo/javaclient/JavaClientTest.java
+++ src/main/java/br/com/devdojo/javaclient/JavaClientTest.java
@@ -1,17 +1,39 @@
-//package br.com.devdojo.javaclient;
-//
-//import java.io.BufferedReader;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//
-//public class JavaClientTest {
-//    public void main(String[] args){
-//        HttpURLConnection connection = null;
-//        BufferedReader reader = null;
-//        try{
-//            URL("htp://localhost:3030"
-//        }catch{
-//
-//        }
-//    }
-//}
+package br.com.devdojo.javaclient;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class JavaClientTest {
+    public void main(String[] args) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        try {
+            URL url = new URL("http://localhost:8080/v1/protected/students/1");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.addRequestProperty("Authorization", "Basic " + encodeUsernamePassword("arturlansoni", "13032001"));
+            reader = new BufferedReader(new InputStreamReader((connection.getInputStream())));
+            StringBuilder jsonSB = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonSB.append(line);
+            }
+            System.out.println(jsonSB.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(reader);
+            if (connection != null)
+                connection.disconnect();
+        }
+    }
+    private static String encodeUsernamePassword(String user, String password) {
+        String userPassword = user + ":" + password;
+        return new String(Base64.encodeBase64(userPassword.getBytes()));
+    }
+}
