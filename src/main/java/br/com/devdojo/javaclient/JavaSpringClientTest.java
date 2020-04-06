a924e9d74535f11d8099b05a7705039ef6ec8e92
Artur Lansoni <arturlansoni@gmail.com>
2020-04-06 09:22:36 -0300

    Create client post request

--- src/main/java/br/com/devdojo/javaclient/JavaSpringClientTest.java
+++ src/main/java/br/com/devdojo/javaclient/JavaSpringClientTest.java
@@ -4,25 +4,28 @@ import br.com.devdojo.model.PageableResponse;
 import br.com.devdojo.model.Student;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 public class JavaSpringClientTest {
     public static void main(String[] args) {
-        RestTemplate restTemplate = new RestTemplateBuilder()
-                .rootUri("http://localhost:8080/v1/protected/students")
-                .basicAuthentication("arturlansoni", "13032001").build();
-        Student student = restTemplate.getForObject("/{id}", Student.class, 1);
-        ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, 1);
-        Student[] students = restTemplate.getForObject("/", Student[].class);
-//        ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
-//                new ParameterizedTypeReference<List<Student>>() {
-//                });
-        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/?sort=id,desc&sort=name,asc", HttpMethod.GET, null,
-                new ParameterizedTypeReference<PageableResponse<Student>>() {
-                });
+
+        Student studentPost = new Student();
+        studentPost.setName("Artur");
+        studentPost.setEmail("artur@gmail.com");
+
+        JavaClientDAO dao = new JavaClientDAO();
+
+        Student studentById = dao.findById(1);
+        System.out.println(studentById);
+
+        List<Student> studentList = dao.listAll();
+        System.out.println(studentList);
+
+        Student save = dao.save(studentPost);
+        System.out.println(save);
+
     }
 }
